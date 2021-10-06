41466b2b0bb1bfd9ee78da5ae4bf8e161a4b9b7f
Romanisae <92027628+Romanisae@users.noreply.github.com>
2021-10-06 14:28:22 +0300

    Update exem3-a.c

--- exem3-a.c
+++ exem3-a.c
@@ -1,1 +1,26 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+float mat(int z[], int n){
+float r=0;
+for (int i=0;i<n;i++)
+{
+r+=z[i];
+}
+
+  r/=n;
+
+  return r;}
+  int main()
+{
+  int n;
+  scanf("%d",&n);
+  int marks[n];
+  for (int i=0;i<n;i++)
+  {
+  scanf("%d",&marks[i])
+  }
+  float x=mat(marks[i],n);
+    if (x>=4.6 )printf("Вы поступили");
+    else printf("Вы не поступили");
+      return 0;
+}
